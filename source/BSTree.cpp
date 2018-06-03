be03c6517aedfd53e17e8b5e3e849af038e54662
MaximZhuravlyov <37067295+MaximZhuravlyov@users.noreply.github.com>
2018-06-03 09:25:27 +0300

    Update BSTree.cpp

--- source/BSTree.cpp
+++ source/BSTree.cpp
@@ -1,161 +1,351 @@
 #include "BSTree.hpp"
 using BSTree::node;
 using BSTree::tree;
-void tree::clean(node*& node)
+template <typename T>
+void tree<T>::clean(node<T>* node)
+{
+	if (node != nullptr) {
+		this->clean(node->left);
+		this->clean(node->right);
+		this->remove(node->data);
+	}
+	node = nullptr;
+	return;
+}
+template <typename T>
+tree<T>::tree(const tree& tree)
+{
+	this->clean(root);
+	stringstream out;
+	out << tree;
+	T value;
+	while (out) 
+	{
+		out >> value;
+		this->insert(value);
+	}
+}
+template <typename T>
+void tree<T>::swap(tree<T>& tree)
+{
+	node<T> node_for_swap = root;
+	this->root = tree.root;
+	tree.root = node_for_swap;
+}
+template <typename T>
+bool tree<T>::insert(T value)
+{
+	if (this->exists(value))
+		return false;
+	if (root == nullptr)
+	{
+		this->root = new node<T> { value };
+		return true;
+	}
+	else
+	{
+		node<T>* new_node = root;
+		while (new_node != nullptr)
+		{
+			if (new_node->data > value)
+			{
+				if (new_node->left == nullptr)
+				{
+					new_node->left = new node<T>{ value };
+					return true;
+				}
+				new_node = new_node->left;
+			}
+			else
+			{
+				if (new_node->right == nullptr)
+				{
+					new_node->right = new node<T>{ value };
+					return true;
+				}
+				new_node = new_node->right;
+			}
+		}
+    }
+	return false;
+}
+template <typename T>
+void tree<T>::print()
+{
+	if (this->root == nullptr)
+	{
+		std::cout << "Дерево пусто\n";
+		return;
+	}
+	if (root == nullptr)
+		return;
+	else
+		print_tree(1,root);
+	return;
+}
+template <typename T>
+void print_tree(int deep,node<T> *node)
 {
 	if (node->left != nullptr)
-		clean(node->left);
+		print_tree(deep + 1, node->left);
+	for (int i = 0; i < deep; i++)
+		cout << "  ";
+	cout << "--" << node->data << endl;
 	if (node->right != nullptr)
-		clean(node->right);
-	delete node;
-	node = nullptr;
+		print_tree(deep + 1, node->right);
 }
-bool tree::exist_next_node(node* node)
+template <typename T>
+void tree<T>::print(BSTree::traversal_order order)
 {
-	if (node->exist == true)
+	if (this->root == nullptr)
+	{
+		cout << "Дерево пусто\n";
+		return;
+	}
+	else
+		line_print(root,std::cout,order);
+		return;
+}
+template <typename T>
+auto line_print(node<T>* node, std::ostream& stream, BSTree::traversal_order order) -> std::ostream& {
+	if (order == BSTree::traversal_order::pre)
+		stream << " " << node->data;
+	if (node->left != nullptr)
+		line_print(node->left, stream, order);
+	if (order == BSTree::traversal_order::in)
+		stream << " " << node->data;
+	if (node->right != nullptr)
+		line_print(node->right, stream, order);
+	if (order == BSTree::traversal_order::post)
+		stream << " " << node->data;
+	return stream;
+}
+template <typename T>
+bool tree<T>::exists(T value)
+{
+	if (root == nullptr)
 		return false;
 	else
-		return true;
-}
-int tree::node_num(node* node)
-{
-	return node->data;
-}
-void tree::inset(node** node1, int val)
-{
-	if ((*node1) == nullptr)
 	{
-		(*node1) = new node;
-		(*node1)->data = val;
-		(*node1)->left = (*node1)->right = nullptr;
-		return;
-	}
-	if ((*node1)->data > val)
-		inset(&(*node1)->left, val);
-	else
-		inset(&(*node1)->right, val);
-}
-void tree::print(node* node, int deep = 0)
-{
-	if (node == nullptr)
-		return;
-	else;
-	{
-		if (node->left != nullptr)
-			print(node->left, deep + 1);
-		for (int i = 0; i < deep; i++)
-			cout << "  ";
-
-		cout << "--" << node->data << endl;
-		if (node->right != nullptr)
-			print(node->right, deep + 1);
+		node<T> *node_ptr = root;
+		bool exst = false;
+		while (node_ptr != nullptr)
+		{
+			if (node_ptr->data == value)
+				exst = true;
+			if (node_ptr->data > value)
+				node_ptr = node_ptr->left;
+			else
+				node_ptr = node_ptr->right;
+		}
+		return exst;
 	}
 }
-void tree::print_cir(node* node, int chose)
+template <typename T>
+bool tree<T>::save(const string& path)
 {
-	if (node == nullptr)
-		return;
-	else;
-	{
-		if (chose == 1)
-			cout << " " << node->data;
-		if (node->left != nullptr)
-			print_cir(node->left, chose);
-		if (chose == 2)
-			cout << " " << node->data;
-		if (node->right != nullptr)
-			print_cir(node->right, chose);
-		if (chose == 3)
-			cout << " " << node->data;
-	}
-}
-bool tree::exist_if_elem(node* node, int chkng_elm)
-{
-	if (find_elem(node, chkng_elm, 0) != 0)
-		return true;
-	else
+	if (root == nullptr)
 		return false;
+	ifstream opening_file(path.c_str());
+	if (!opening_file.is_open()) {
+		ofstream file_(path.c_str());
+		file_ << *this;
+		file_.close();
+		return true;
+	}
+	else {
+		string answer;
+		cout << "Файл уже существует, перезаписать ? (Да|Нет):\n";
+		while ( (cin >> answer) && !(answer == "да" || answer == "ДА" || answer == "Да"
+			|| answer == "yes" || answer == "Yes" || answer == "YES" || answer == "y" || answer == "Y" || 
+			answer == "НЕТ" || answer == "Нет" || answer == "нет" || answer == "no" ||
+			answer == "No" || answer == "NO" || answer == "n" || answer == "N") )
+		{
+			cin.clear();
+			cin.ignore(32, '\n');
+			cout << "Введите \"yes\" если хотите перезаписать файл, \"no\" - если нет.\n";
+		}
+		if (answer == "да" || answer == "ДА" || answer == "Да" || answer == "yes" || answer == "Yes" || answer == "YES" || answer == "y" || answer == "Y")
+		{
+			ofstream file_(path.c_str());
+			file_ << *this;
+			file_.close();
+			return true;
+		}
+		else if (answer == "НЕТ" || answer == "Нет" || answer == "нет" || answer == "no" || answer == "No" || answer == "NO" || answer == "n" || answer == "N")
+			return false;
+	}
 }
-int tree::find_elem(node* node, int chkng_elm, int exst_elm)
+template <typename T>
+bool tree<T>::load(const string& path)
 {
-	if (node->data == chkng_elm)
-		exst_elm++;
-	if (node == nullptr)
-		return exst_elm;
+	ifstream opening_file(path.c_str());
+	if (!opening_file.is_open())
+		return false;
+	cout << "clean\n\n";
+	this->clean(root);
+	T value;
+	opening_file >> value;
+	if (!opening_file)
+	{
+		opening_file.close();
+		return false;
+	}
 	else
-	{
-		if (node->left != nullptr)
-			exst_elm = find_elem(node->left, chkng_elm, exst_elm);
-		if (node->right != nullptr)
-			exst_elm = find_elem(node->right, chkng_elm, exst_elm);
-	}
-	return exst_elm;
+		while (opening_file)
+		{
+			this->insert(value);
+			opening_file >> value;
+		}
+	opening_file.close();
+	return true;
 }
-int tree::delete_elem(node* node1, int del_elm, node* lastnode = nullptr, bool last_node_right = true, node* new_root = nullptr, int elem_quantity = 1)
+template <typename T>
+bool tree<T>::remove(T value)
 {
-	if (node1->data == del_elm  && elem_quantity == 1)
+	if (root == nullptr)
+		return false;
+	if (!(this->exists(value)))
+		return false;
+	else if (this->root->right == nullptr && this->root->left == nullptr)
 	{
-		clean(this->root);
+		root->data = 0;
+		root = nullptr;
+		return true;
 	}
-	if (node1->data == del_elm)
+	else if (root->data == value)
 	{
-		if (node1->left == nullptr && node1->right == nullptr)
+		node<T> * deleted_node = root;
+		if (root->left != nullptr && root->right == nullptr)
+			this->root = root->left;
+		else if (root->left == nullptr && root->right != nullptr)
+			this->root = root->right;
+		else
 		{
-			if (last_node_right)
-				lastnode->right = nullptr;
-			else
-				lastnode->left = nullptr;
-			delete node1;
-			return 0;
-		}
-		if (node1->left != nullptr && node1->right == nullptr)
-		{
-			if (last_node_right)
-				lastnode->right = node1->left;
-			else
-				lastnode->left = node1->left;
-			delete node1;
-			return 0;
-		}
-		if (node1->left == nullptr && node1->right != nullptr)
-		{
-			if (last_node_right)
-				lastnode->right = node1->right;
-			else
-				lastnode->left = node1->right;
-			delete node1;
-			return 0;
-		}
-		if (node1->left != nullptr && node1->right != nullptr)
-		{
-			if (last_node_right)
+			node<T> * left_root_ptr = root->left;
+			root = root->right;
+			node<T> * node_ptr = root;
+			while (node_ptr->left != left_root_ptr)
 			{
-				if (node_num(node1->left) > lastnode->data)
-					lastnode->right = node1->left;
+				if (node_ptr->left == nullptr)
+					node_ptr->left = left_root_ptr;
 				else
-					lastnode->right = node1->right;
+					node_ptr = node_ptr->left;
+			}
+			/*while (left_root_ptr != nullptr)
+			{
+				if (left_root_ptr->left == nullptr)
+					left_root_ptr = left_root_ptr->left;
+				else
+					left_root_ptr = left_root_ptr->right;
+			}
+			delete left_root_ptr;
+			while (node_ptr != nullptr)
+			{
+				if (node_ptr->left == nullptr)
+					node_ptr = node_ptr->left;
+				else
+					node_ptr = node_ptr->right;
+			}
+			delete node_ptr;*/
+		}
+		delete deleted_node;
+	}
+	else
+	{
+		node<T> *deleted_node = root;
+		node<T> *node_before_deleted_node;
+		bool last_turn_left;
+		while (deleted_node->data != value)
+		{
+			node_before_deleted_node = deleted_node;
+			if (deleted_node->data > value)
+			{
+				last_turn_left = true;
+				deleted_node = deleted_node->left;
 			}
 			else
 			{
-				if (node_num(node1->left) > lastnode->data)
-					lastnode->left = node1->left;
-				else
-					lastnode->left = node1->right;
+				last_turn_left = false;
+				deleted_node = deleted_node->right;
 			}
-			new_root = node1->right;
-			delete node1;
-			return 1;
 		}
-
+		if (deleted_node->left == nullptr && deleted_node->right == nullptr)
+		{
+			if (last_turn_left)
+				node_before_deleted_node->left = nullptr;
+			else
+				node_before_deleted_node->right = nullptr;
+			return true;
+		}
+		else if (last_turn_left)
+		{
+			if (deleted_node->right != nullptr)
+			{
+				node_before_deleted_node->left = deleted_node->right;
+				if (deleted_node->left != nullptr)
+				{
+					node<T> * node_ptr = deleted_node->left;
+					node_before_deleted_node = node_before_deleted_node->left;
+					while (node_before_deleted_node->left != node_ptr)
+					{
+						if (node_before_deleted_node->left == nullptr)
+							node_before_deleted_node->left = node_ptr;
+						else
+							node_before_deleted_node = node_before_deleted_node->left;
+					}
+					/*while (node_ptr != nullptr)
+					{
+						if (node_ptr->left == nullptr)
+							node_ptr = node_ptr->left;
+						else
+							node_ptr = node_ptr->right;
+					}
+					delete node_ptr;*/
+				}
+			}
+			else
+				node_before_deleted_node->left = deleted_node->left;
+		}
+		else  if (!last_turn_left)
+		{
+			if (deleted_node->left != nullptr)
+			{
+				node_before_deleted_node->right = deleted_node->left;
+				if (deleted_node->right != nullptr)
+				{
+					node<T> * node_ptr = deleted_node->right;
+					node_before_deleted_node = node_before_deleted_node->right;
+					while (node_before_deleted_node->right != node_ptr)
+					{
+						if (node_before_deleted_node->right == nullptr)
+							node_before_deleted_node->right = node_ptr;
+						else
+							node_before_deleted_node = node_before_deleted_node->right;
+					}
+					/*while (node_ptr != nullptr)
+					{
+						if (node_ptr->left == nullptr)
+							node_ptr = node_ptr->left;
+						else
+							node_ptr = node_ptr->right;
+					}
+					delete node_ptr;*/
+				}
+			}
+			else
+				node_before_deleted_node->right = deleted_node->right;
+		}
+		/*while (node_before_deleted_node != nullptr)
+		{
+			if (node_before_deleted_node->left == nullptr)
+				node_before_deleted_node = node_before_deleted_node->left;
+			else
+				node_before_deleted_node = node_before_deleted_node->right;
+		}
+		delete node_before_deleted_node;
+		delete deleted_node;*/
+		return true;
 	}
-	else if (node1 == nullptr)
-		return 0;
-	else
-	{
-		if (node1->left != nullptr && new_root == nullptr)
-			delete_elem(node1->left, del_elm, node1, false, new_root);
-		if (node1->right != nullptr)
-			delete_elem(node1->right, del_elm, node1, true, new_root);
-	}
-	return 0;
+	return false;
 }
